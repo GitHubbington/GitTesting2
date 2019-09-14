c3b53f3cc0cd47c8e19ecda062e0c651589b6ccb
GitHubbington <nathanpolley27@gmail.com>
2019-09-13 20:41:21 -0400

    Initial Commit

---
+++ GitTesting2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTesting2
+//
+//  Created by Nathan Polley on 2019-09-13.
+//  Copyright © 2019 Nathan Polley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
